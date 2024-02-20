20379283eb24e4dc9d8b942c53fee9613364345b
serhernandez <sergio.hernandez@cgi.com>
2024-02-20 09:38:03 -0600

    Add sampling to languages spanning more than 20 years

--- home.py
+++ home.py
@@ -11,22 +11,27 @@ plt.close("all")
 
 pd.options.display.float_format = '{:.0f}'.format
 
-movie_df = pd.read_csv('tmdb_5000_movies.csv')
-movie_df.dropna()
-credit_df = pd.read_csv('tmdb_5000_credits.csv')
-credit_df.dropna()
+app = Flask(__name__)
 
-combined_df = movie_df.merge(credit_df, how='inner', left_on='id', right_on='movie_id')
-trimmed_df = combined_df[combined_df['budget'] > 0].drop(['homepage', 'keywords', 'overview', 'tagline', 'movie_id', 'title_y'], axis=1).sort_values(by='budget', ascending=False)
+@app.route("/", methods=['GET', 'POST'])
+def graph():
 
-trimmed_df = trimmed_df.query('original_language != "xx"') #Ignoring xx as this is considered "no language"
-trimmed_df['release_year'] = trimmed_df['release_date'].apply(lambda x: x.split("-")[0])
-trimmed_df['crew_count'] = trimmed_df['crew'].apply(lambda x: len(json.loads(x)))
-trimmed_df['cast_count'] = trimmed_df['cast'].apply(lambda x: len(json.loads(x)))
-trimmed_df['credits_count'] = trimmed_df['crew_count'] + trimmed_df['cast_count']
-trimmed_df = trimmed_df.replace('cn', 'zh') #Replacing instances of cn with zh as cn is an invalid ISO639-1 code and should be zh or zh-cn
+    movie_df = pd.read_csv('tmdb_5000_movies.csv')
+    movie_df.dropna()
+    credit_df = pd.read_csv('tmdb_5000_credits.csv')
+    credit_df.dropna()
 
-budget_info = trimmed_df.groupby(['original_language', 'release_year'], as_index=False).agg(
+    combined_df = movie_df.merge(credit_df, how='inner', left_on='id', right_on='movie_id')
+    trimmed_df = combined_df[combined_df['budget'] > 0].drop(['homepage', 'keywords', 'overview', 'tagline', 'movie_id', 'title_y'], axis=1).sort_values(by='budget', ascending=False)
+
+    trimmed_df = trimmed_df.query('original_language != "xx"') #Ignoring xx as this is considered "no language"
+    trimmed_df['release_year'] = trimmed_df['release_date'].apply(lambda x: x.split("-")[0])
+    trimmed_df['crew_count'] = trimmed_df['crew'].apply(lambda x: len(json.loads(x)))
+    trimmed_df['cast_count'] = trimmed_df['cast'].apply(lambda x: len(json.loads(x)))
+    trimmed_df['credits_count'] = trimmed_df['crew_count'] + trimmed_df['cast_count']
+    trimmed_df = trimmed_df.replace('cn', 'zh') #Replacing instances of cn with zh as cn is an invalid ISO639-1 code and should be zh or zh-cn
+
+    budget_info = trimmed_df.groupby(['original_language', 'release_year'], as_index=False).agg(
     avg_budget=pd.NamedAgg('budget', 'mean'), 
     avg_credits=pd.NamedAgg('credits_count', 'mean'),
     avg_cast=pd.NamedAgg('cast_count', 'mean'),
@@ -34,32 +39,32 @@ budget_info = trimmed_df.groupby(['original_language', 'release_year'], as_index
     avg_runtime=pd.NamedAgg('runtime', 'mean'),
     avg_revenue=pd.NamedAgg('revenue', 'mean')).sort_values(by=['original_language', 'release_year'])
 
-for lang in budget_info['original_language'].unique():
-    if len(budget_info[budget_info['original_language'] == lang]) == 1:
-        budget_info = budget_info.query(f'original_language != "{lang}"') #Dropping languages with only 1 year for the sake of graph readability
+    for lang in budget_info['original_language'].unique():
+        if len(budget_info[budget_info['original_language'] == lang]) == 1:
+            budget_info = budget_info.query(f'original_language != "{lang}"') #Dropping languages with only 1 year for the sake of graph readability
 
-budget_info['avg_budget'] = budget_info['avg_budget'] / 1000000
-budget_info['avg_revenue'] = budget_info['avg_revenue'] / 1000000
+    budget_info['avg_budget'] = budget_info['avg_budget'] / 1000000
+    budget_info['avg_revenue'] = budget_info['avg_revenue'] / 1000000
 
-langs = {Lang(lang).name:lang for lang in budget_info['original_language'].unique()}
-secondaries = {
-    'cast': ("Average # of Cast Members", "avg_cast"),
-    'credits': ("Average # of Credits", "avg_credits"),
-    'crew': ("Average # of Crew Members", "avg_crew"),
-    'revenue': ("Average Revenue in USD (in Millions)", "avg_revenue"),
-    'runtime': ("Average Runtime (in Minutes)", "avg_runtime")
-}
+    langs = {Lang(lang).name:lang for lang in budget_info['original_language'].unique()}
+    secondaries = {
+        'cast': ("Average # of Cast Members", "avg_cast"),
+        'credits': ("Average # of Credits", "avg_credits"),
+        'crew': ("Average # of Crew Members", "avg_crew"),
+        'revenue': ("Average Revenue in USD (in Millions)", "avg_revenue"),
+        'runtime': ("Average Runtime (in Minutes)", "avg_runtime")
+    }
 
-app = Flask(__name__)
-
-@app.route("/", methods=['GET', 'POST'])
-def graph():
     if request.method == "POST":
         lang = langs[request.form.get("language")]
         secondary = request.form.get("secondary")
     else:
         lang = "en"
         secondary = "credits"
+
+    if len(budget_info[budget_info['original_language'] == lang]) > 20:
+        budget_info = budget_info[budget_info['original_language'] == lang]
+        budget_info = pd.concat([budget_info.head(1), budget_info.iloc[1:-1].sample(n=18), budget_info.tail(1)]).sort_values(by='release_year')
 
     fig, ax1 = plt.subplots()
     ax1.set_xlabel('Release Year')
@@ -75,7 +80,7 @@ def graph():
 
     fig.legend()
 
-    fig.set_size_inches(20, 15)
+    fig.set_size_inches(16, 12)
     plt.savefig('static/images/plot.png', bbox_inches="tight", dpi=100)
 
     return render_template('test.html', lang=Lang(lang).name, langs=sorted(langs.keys()), url='/static/images/plot.png')
